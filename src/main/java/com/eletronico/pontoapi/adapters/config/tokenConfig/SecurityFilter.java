4192ea275d74ae0b39fe509615146e408c9def98
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-02-28 23:19:04 -0300

    configuration security filter to token user

---
+++ src/main/java/com/eletronico/pontoapi/adapters/config/tokenConfig/SecurityFilter.java
@@ -0,0 +1,45 @@
+package com.eletronico.pontoapi.adapters.config.tokenConfig;
+
+import com.eletronico.pontoapi.adapters.database.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.IOException;
+
+public class SecurityFilter {
+
+    private TokenAccessUser tokenAccessUser;
+
+    private UserRepository userRepository;
+
+    SecurityFilter(TokenAccessUser tokenAccessUser, UserRepository userRepository){
+        this.tokenAccessUser = tokenAccessUser;
+        this.userRepository = userRepository;
+    }
+
+    //@Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var login = tokenAccessUser.validateToken(token);
+            UserDetails userDetails = userRepository.findByEmail(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request,response);
+
+    }
+
+    public String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return   null;
+        return authHeader.replace("Bearer " , "");
+
+    }
+}
