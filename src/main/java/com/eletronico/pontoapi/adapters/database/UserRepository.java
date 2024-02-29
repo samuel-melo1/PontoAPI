c6f2801e8df4a7f737c612db5af13dd957893a01
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-02-28 23:19:31 -0300

    implemented method to search user by email

--- src/main/java/com/eletronico/pontoapi/adapters/database/UserRepository.java
+++ src/main/java/com/eletronico/pontoapi/adapters/database/UserRepository.java
@@ -2,6 +2,9 @@ package com.eletronico.pontoapi.adapters.database;
 
 import com.eletronico.pontoapi.core.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    UserDetails findByEmail(String login);
 }
