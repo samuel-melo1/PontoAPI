f7ede11ec1100377d64b0929beb38f72ff31305c
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-02-28 23:16:03 -0300

    configuration token service to use autentication stateless

---
+++ src/main/java/com/eletronico/pontoapi/adapters/config/tokenConfig/TokenAccessUser.java
@@ -0,0 +1,49 @@
+package com.eletronico.pontoapi.adapters.config.tokenConfig;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.eletronico.pontoapi.core.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class TokenAccessUser {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("library-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error while generating token", exception);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("library-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTCreationException exception){
+            return "";
+        }
+    }
+    public Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
