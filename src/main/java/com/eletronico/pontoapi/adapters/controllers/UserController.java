9227fca53f68136660881c355640ee53ac77339f
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-02-29 23:27:15 -0300

    created user controller

---
+++ src/main/java/com/eletronico/pontoapi/adapters/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.eletronico.pontoapi.adapters.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apí/v1/")
+public class UserController {
+}
