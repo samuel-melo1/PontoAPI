235b79c6cf29af3b3dd42701e39da602b556cbc2
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-04-03 22:43:27 -0300

    configuration cors and client login

---
+++ src/main/java/com/eletronico/pontoapi/adapters/config/security/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.eletronico.pontoapi.adapters.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
