6c21aa76a8f1ad2f62e611d1926672cba8a0586f
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-05-06 22:58:40 -0300

    implementation position service, repository and controller

---
+++ src/main/java/com/eletronico/pontoapi/core/services/PositionService.java
@@ -0,0 +1,25 @@
+package com.eletronico.pontoapi.core.services;
+
+import com.eletronico.pontoapi.adapters.database.PositionRepository;
+import com.eletronico.pontoapi.core.dto.PositionDTO;
+import com.eletronico.pontoapi.utils.mapper.MapperDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class PositionService {
+    private static final Logger LOG = LoggerFactory.getLogger(PositionService.class.getName());
+    private PositionRepository repository;
+    public PositionService(PositionRepository repository){
+        this.repository = repository;
+    }
+    public List<PositionDTO> findAll(){
+        LOG.info("Finding all people!");
+        return MapperDTO.parseListObjects(repository.findAll(), PositionDTO.class);
+    }
+}
