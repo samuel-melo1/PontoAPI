16a5938e68b3696f1efa9030823acaafab47691a
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-05-15 22:57:30 -0300

    implementation role entity and role dto

---
+++ src/main/java/com/eletronico/pontoapi/core/role/dto/RoleDTO.java
@@ -0,0 +1,14 @@
+package com.eletronico.pontoapi.core.role.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDTO {
+
+    private Integer code;
+    private String name;
+}
