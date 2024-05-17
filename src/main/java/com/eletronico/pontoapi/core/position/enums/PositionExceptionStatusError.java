3e3c8acb3fbe417c478b4b0aaa7896b5cd461f68
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-05-17 19:15:22 -0300

    implementation exception handler position

--- src/main/java/com/eletronico/pontoapi/core/position/enums/PositionExceptionStatusError.java
+++ src/main/java/com/eletronico/pontoapi/core/position/enums/PositionExceptionStatusError.java
@@ -8,8 +8,8 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @Getter
 @AllArgsConstructor
 public enum PositionExceptionStatusError {
-    ALREDY_EXIST("000", "Section alredy Exist. Please, try other!", CONFLICT),
-    NOT_FOUND_POSITION("001","Section Not Found!", NOT_FOUND);
+    ALREDY_EXIST("000", "Position alredy Exist. Please, try other!", CONFLICT),
+    NOT_FOUND_POSITION("001","Position Not Found!", NOT_FOUND);
 
     private final String code;
     private final String message;
--- src/main/java/com/eletronico/pontoapi/core/position/exceptions/PositionNotFoundException.java
+++ src/main/java/com/eletronico/pontoapi/core/position/exceptions/PositionNotFoundException.java
@@ -1,13 +1,14 @@
 package com.eletronico.pontoapi.core.position.exceptions;
 
 import com.eletronico.pontoapi.core.position.enums.PositionExceptionStatusError;
-import com.eletronico.pontoapi.core.sector.enums.SectionExceptionStatusError;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+
 @Getter
-public class PositionAlredyExistException extends RuntimeException{
+public class PositionNotFoundException extends RuntimeException {
+
     private final HttpStatus status;
-    public PositionAlredyExistException(PositionExceptionStatusError error){
+    public PositionNotFoundException(PositionExceptionStatusError error){
         super(error.getMessage());
         this.status = error.getStatus();
     }
