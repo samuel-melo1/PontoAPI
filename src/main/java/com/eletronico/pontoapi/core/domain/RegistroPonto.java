045fc4cbf9f0d78bf8cbaa523223b35179ddd53f
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-10-21 21:58:21 -0300

    created and implemented registroPonto class

--- src/main/java/com/eletronico/pontoapi/core/domain/RegistroPonto.java
+++ src/main/java/com/eletronico/pontoapi/core/domain/RegistroPonto.java
@@ -1,14 +1,14 @@
 package com.eletronico.pontoapi.core.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.eletronico.pontoapi.core.enums.TipoRegistroPonto;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -19,6 +19,13 @@ public class RegistroPonto implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_registro;
+    private Long id_ponto;
 
+    private LocalDateTime dateRegister;
+    private String currentLocation;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User id_user;
+    private LocalDateTime dateUpdate;
+    private TipoRegistroPonto type;
 }
